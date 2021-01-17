3cf201e17b2baa148dd45e949c87bff872173dab
Tom White <fredley@gmail.com>
2021-01-17 20:44:18 +0000

    v2

--- update.sh
+++ update.sh
@@ -5,7 +5,12 @@ aws s3 cp style.css s3://$BUCKET
 aws s3 cp favicon.ico s3://$BUCKET
 aws s3 cp ac.png s3://$BUCKET
 
+echo "Updating ACLs..."
+
 aws s3api put-object-acl --bucket $BUCKET --key index.html --acl public-read
 aws s3api put-object-acl --bucket $BUCKET --key style.css --acl public-read
 aws s3api put-object-acl --bucket $BUCKET --key favicon.ico --acl public-read
 aws s3api put-object-acl --bucket $BUCKET --key ac.png --acl public-read
+
+echo "Invalidating caches..."
+aws cloudfront create-invalidation --distribution-id E1U55MPGMB8CN2 --paths "/*"
